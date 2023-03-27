6e979099c2fd601f082f416ca9304acd483a7008
Neil Coplin <neil.coplin@gmail.com>
2023-03-26 21:36:56 -0400

    Making a fun 404 page with CatAPI

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,31 @@
 import Head from 'next/head'
-import { Container, Stack } from "react-bootstrap";
+import { Container, Image, Stack } from "react-bootstrap";
+import { useState, useEffect} from 'react'
 
 export default function Custom404(props:any) {
+  const [catSrc, setCatSrc] = useState("loading.gif");
+
+
+  useEffect(() => {
+    fetch("https://api.thecatapi.com/v1/images/search")
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        }
+        throw response;
+      })
+      .then(responseBody => {
+        return JSON.parse(responseBody);
+      })
+      .then(data => {
+        console.log(data);
+        setCatSrc(data[0].url);
+      })
+      .catch(error => {
+        console.error("Error in fetching a random cat picture: ", error);
+      });
+  }, [])
+
   return (
     <>
       <Head>
@@ -10,9 +34,13 @@ export default function Custom404(props:any) {
       <main>
         <Container fluid="true" id="not-found">
           <Stack gap={3}>
-            <Container fluid="md">
-              <h1>404</h1>
-              <p>We&apos;re sorry, but the page you were looking for doesn&apos;t exist.</p>
+            <Container fluid="md" className='top-padding center'>
+              <h1>Page Not Found</h1>
+              <p>The page you requested could not be found, so here is a  random picture of a cat instead.</p>
+ 
+              <Container className="contain">
+                <Image src={catSrc} alt="A random cat picture from the internet" fluid />
+              </Container>
             </Container>
           </Stack>
         </Container>
