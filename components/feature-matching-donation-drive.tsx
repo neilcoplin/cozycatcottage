9dae45d9b8c9744af2cd7be08028108e409233ce
Neil Coplin <neil.coplin@gmail.com>
2025-08-07 08:36:58 -0400

    Fixing link...

--- components/feature-matching-donation-drive.tsx
+++ components/feature-matching-donation-drive.tsx
@@ -11,7 +11,7 @@ export default function FeatureMatchingDonationDrive() {
           <h2>5th Annual Matching Funds Campaign</h2>
           <p className={styles.largeParagraph}><strong>Double your donation this August and help us raise <span className={styles.brownText}>$20,000</span>!</strong></p>
           <p>A generous supporter will match donations up to our goal of $10,000. This major fundraiser is a great opportunity to help Cozy Cat Cottage continue its mission of rescuing and adoptiong hundreds of homeless, sick, and abused cats and kittens annually.</p>
-          <Button href="paypal.com/us/fundraiser/charity/1803839" size="lg" className={styles.buttonOrange} target="_blank" rel="noreferrer">Donate via Paypal</Button>
+          <Button href="https://paypal.com/us/fundraiser/charity/1803839" size="lg" className={styles.buttonOrange} target="_blank" rel="noreferrer">Donate via Paypal</Button>
           <p>The first 50 donors who donate $100 or more will receive a custom painting made by a current or previous Cozy Cat resident.</p>
         </Col>
         <Col className={styles.center}>
